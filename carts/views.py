d4f0104ed7ed031e85e16064a7a34a8bf466a434
Johann <jbquispe@hotmail.com>
2020-08-17 22:43:31 -0400

    adding template view for list carts

--- carts/views.py
+++ carts/views.py
@@ -1,14 +1,19 @@
 from django.shortcuts import render
-
+from .models import Cart
+from products.models import Product
+from .utils import get_or_create_cart
 # Create your views here.
 def cart(request):
-
-    #request.session['cart_id']='123'
-    valor = request.session.get('cart_id')
-    print(valor)
-
-    request.session['cart_id']= None
-
+    #request.session['cart_id'] =  None
+    cart = get_or_create_cart(request)
     return render (request,'carts/cart.html',{
+        'cart':cart
+    })
+def add(request):
+    cart = get_or_create_cart(request)
+    product = Product.objects.get(pk=request.POST.get('product_id'))
 
+    cart.products.add(product)
+    return render(request,'carts/add.html',{
+        'product': product
     })
