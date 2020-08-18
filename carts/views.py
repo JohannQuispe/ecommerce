b629597ed48f13fb349b4048493186ea2f3a0b54
Johann <jbquispe@hotmail.com>
2020-08-18 14:07:29 -0400

    update list of cart

--- carts/views.py
+++ carts/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from .models import Cart
+from django.shortcuts import redirect
 from products.models import Product
 from .utils import get_or_create_cart
+from django.shortcuts import get_object_or_404
 # Create your views here.
 def cart(request):
     #request.session['cart_id'] =  None
@@ -11,9 +13,14 @@ def cart(request):
     })
 def add(request):
     cart = get_or_create_cart(request)
-    product = Product.objects.get(pk=request.POST.get('product_id'))
+    product = get_object_or_404(Product, pk=request.POST.get('product_id'))
 
     cart.products.add(product)
     return render(request,'carts/add.html',{
         'product': product
     })
+def remove(request):
+    cart = get_or_create_cart(request)
+    product = get_object_or_404(Product, pk=request.POST.get('product_id'))
+    cart.products.remove(product)
+    return redirect('carts:cart')
