a366c676b17ce03d550ec8b3e6b4d594842a42d9
Johann <jbquispe@hotmail.com>
2020-08-19 11:12:25 -0400

    adding model direccion de envio

--- amboro_store/settings.py
+++ amboro_store/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'orders',
     'products',
     'categories',
+    'shipping_addresses',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
