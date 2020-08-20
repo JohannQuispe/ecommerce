426bafadd6e6a9066dd3b1653523d0693b221d98
Johann <jbquispe@hotmail.com>
2020-08-19 23:42:37 -0400

    adding Styles

--- shipping_addresses/views.py
+++ shipping_addresses/views.py
@@ -2,8 +2,11 @@ from django.contrib import messages
 from django.shortcuts import render
 from django.shortcuts import redirect
 from django.views.generic import ListView
+from django.views.generic.edit import UpdateView
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.messages.views import SuccessMessageMixin
+from django.shortcuts import reverse
 
 from .models import ShippingAddress
 from .forms import ShippingAddressForm
@@ -12,9 +15,27 @@ class ShippingAddressListView(LoginRequiredMixin, ListView):
     login_url = 'login'
     model = ShippingAddress
     template_name = 'shipping_addresses/shipping_addresses.html'
+    success_message = 'Dirección actualizada exitosamente'
 
     def get_queryset(self):
         return ShippingAddress.objects.filter(user=self.request.user).order_by('-default')
+
+class ShippingAddressUpdateView(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
+    login_url = 'login'
+    model = ShippingAddress
+    form_class = ShippingAddressForm
+    template_name = 'shipping_addresses/update.html'
+    success_message = 'Dirección actualizada exitosamente'
+
+
+    def get_success_url(self):
+        return reverse('shipping_addresses:shipping_addresses')
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.id != self.get_object().user_id:
+            return redirect('carts:cart')
+
+        return super(ShippingAddressUpdateView, self).dispatch(request, *args, **kwargs)
 
 @login_required(login_url='login')
 def create(request):
