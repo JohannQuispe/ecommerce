7c593854d3bec2e19450d8a2897bb459cbc59eca
Johann <jbquispe@hotmail.com>
2020-08-22 18:09:40 -0400

    change dbase

--- amboro_store/settings.py
+++ amboro_store/settings.py
@@ -84,8 +84,12 @@ WSGI_APPLICATION = 'amboro_store.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'amboro_store',
+        'USER': 'postgres',
+        'PASSWORD': 'passwd',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
