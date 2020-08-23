8b417954df0b7f5a5697baa9f87c260476d34f62
Johann <jbquispe@hotmail.com>
2020-08-23 10:12:12 -0400

    ss

--- amboro_store/settings.py
+++ amboro_store/settings.py
@@ -140,10 +140,11 @@ STRIPE_PRIVATE_KEY = 'sk_test_51HImxHGU4eiL1A640dpmLiI6zvqdULk7vyilaFghUBichHxtt
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
 
-)
+#STATICFILES_DIRS = (
+#    os.path.join(BASE_DIR, 'static'),
+
+#)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
